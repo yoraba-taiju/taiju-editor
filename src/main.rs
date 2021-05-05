900fb0de7d06b38c83d9efc33f507e1bc1c04a4b
Kaede Fujisaki <kaede@hexe.net>
2021-05-05 18:44:25 +0900

    Create menu

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,39 @@ fn main() {
     App::build()
       .add_plugins(DefaultPlugins)
       .add_plugin(EguiPlugin)
+      .add_startup_system(startup.system())
       .add_system(ui_example.system())
       .run();
 }
 
-fn ui_example(mut egui_context: ResMut<EguiContext>) {
-    egui::Window::new("Hello").show(egui_context.ctx(), |ui| {
+fn startup(
+
+) {
+
+}
+
+fn ui_example(mut egui_ctx: ResMut<EguiContext>) {
+    egui::TopPanel::top("top_panel").show(egui_ctx.ctx(), |ui| {
+        egui::menu::bar(ui, |ui| {
+            egui::menu::menu(ui, "File", |ui| {
+                ui.label("Open Scene");
+                ui.indent("Open Source", |ui| {
+                    if ui.button("Stage01").clicked() {
+                    }
+                });
+                ui.separator();
+                if ui.button("Save Scene").clicked() {
+                }
+                ui.separator();
+                if ui.button("Quit").clicked() {
+                    std::process::exit(0);
+                }
+            });
+        });
+    });
+    egui::Window::new("Hello")
+      .default_pos((100.0, 100.0))
+      .show(egui_ctx.ctx(), |ui| {
         ui.label("world");
     });
 }

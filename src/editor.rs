e23607d14e22fe7cb81d5835548b2b9a4018afa6
Kaede Fujisaki <kaede@hexe.net>
2021-05-09 17:35:52 +0900

    マップが移動できるようになった

--- src/editor.rs
+++ src/editor.rs
@@ -1,18 +1,145 @@
-use bevy::prelude::*;
-use bevy_egui::egui;
+use std::ops::DerefMut;
+use bevy::{
+  input::mouse::{MouseMotion, MouseWheel},
+  prelude::*,
+};
+use bevy_egui::{egui, egui::Event, EguiContext};
+use taiju::chapter::prelude::*;
 
 pub struct Map;
 
 pub struct Editor {
   pub map_id: Entity,
+  // map_handling
+  pub map_scale: f32,
+  pub mouse_pos: Vec2,
+  pub drag_start_mouse_pos: Vec2,
+  pub drag_start_map_pos: Vec2,
+  // 
   pub menu_pos: Option<(f32, f32)>,
 }
 
 impl Editor {
-  pub(crate) fn new(map_id: Entity) -> Self {
-    Self {
+  pub(crate) fn spawn(mut commands: Commands) {
+    let map_id = commands
+      .spawn()
+      .insert(Map)
+      .insert(Transform::identity())
+      .insert(GlobalTransform::identity())
+      .id();
+    commands.insert_resource(Self {
       map_id,
+      map_scale: 1.0,
+      mouse_pos: Default::default(),
+      drag_start_mouse_pos: Default::default(),
+      drag_start_map_pos: Default::default(),
       menu_pos: Default::default(),
+    });
+  }
+  pub(crate) fn update_map(
+    mut commands: Commands,
+    mouse_button_input: Res<Input<MouseButton>>,
+    mut mouse_wheel_events: EventReader<MouseWheel>,
+    mut cursor_moved_events: EventReader<CursorMoved>,
+    mut editor_res: ResMut<Editor>,
+    enemy_server: Res<EnemyServer>,
+    mut map_query: Query<(Entity, &mut Transform), With<Map>>,
+  ) {
+    let mut e = editor_res.deref_mut();
+    let (map_id, mut map_trans) = map_query.single_mut().unwrap();
+    // zoom map
+    for event in mouse_wheel_events.iter() {
+      e.map_scale += event.y / 5.0;
+      if e.map_scale < 0.2 {
+        e.map_scale = 0.2;
+      }
+      map_trans.scale.x = e.map_scale;
+      map_trans.scale.y = e.map_scale;
+      map_trans.scale.z = e.map_scale;
+    }
+    // move map
+    for event in cursor_moved_events.iter() {
+      e.mouse_pos = event.position;
+    }
+    if mouse_button_input.just_pressed(MouseButton::Middle) {
+      e.drag_start_map_pos.x = map_trans.translation.x;
+      e.drag_start_map_pos.y = map_trans.translation.y;
+      e.drag_start_mouse_pos = e.mouse_pos;
+    } else if mouse_button_input.pressed(MouseButton::Middle) {
+      map_trans.translation.x = e.drag_start_map_pos.x + (e.mouse_pos.x - e.drag_start_mouse_pos.x);
+      map_trans.translation.y = e.drag_start_map_pos.y + (e.mouse_pos.y - e.drag_start_mouse_pos.y);
+    }
+    // modify map
+    if mouse_button_input.just_pressed(MouseButton::Right) {
+      let spr = enemy_server.sprites[&EnemyBody::Enemy01].clone();
+      //spr.transform.translation.x = 
+      commands.spawn().insert_bundle(spr).insert(Parent(map_id));
+    }
+  }
+  pub(crate) fn update_ui (
+    mut commands: Commands,
+    egui_ctx: Res<EguiContext>,
+    mut editor: ResMut<Editor>,
+    clock: Res<ClockRef>,
+    asset_server: Res<AssetServer>,
+  ) {
+    let input: &egui::InputState = egui_ctx.ctx().input();
+    for ev in input.events.iter() {
+      match ev {
+        &Event::Copy => {}
+        &Event::Cut => {}
+        Event::Text(str) => {}
+        &Event::Key {
+          key: Key,
+          pressed: bool,
+          modifiers: Modifiers,
+        } => {}
+        &Event::PointerMoved(pos) => {}
+        &Event::PointerButton {
+          pos,
+          button,
+          pressed,
+          modifiers,
+        } => {
+          if button == egui::PointerButton::Secondary {
+            if pressed {
+              editor.menu_pos = Some((pos.x, pos.y));
+            }
+          }
+        }
+        &Event::PointerGone => {}
+      }
+    }
+    egui::TopPanel::top("top_panel").show(egui_ctx.ctx(), |ui| {
+      egui::menu::bar(ui, |ui| {
+        egui::menu::menu(ui, "File", |ui| {
+          ui.label("Open Scene");
+          ui.indent("Open Source", |ui| {
+            if ui.button("Stage01").clicked() {
+              commands.remove_resource::<Handle<Scenario>>();
+              let handle = asset_server.load::<Scenario, _>("scenario/stage01.ron");
+              commands.insert_resource(handle);
+            }
+          });
+          ui.separator();
+          if ui.button("Save Scene").clicked() {
+          }
+          ui.separator();
+          if ui.button("Quit").clicked() {
+            std::process::exit(0);
+          }
+        });
+      });
+    });
+  
+    let mut open = true;
+    if let Some(pos) = editor.menu_pos {
+      egui::Window::new("Hello")
+        .open(&mut open)
+        .default_pos(pos)
+        .show(egui_ctx.ctx(), |ui| {
+          ui.label("world");
+        });
     }
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use bevy::prelude::*;
-use bevy_egui::{egui, egui::{InputState, Event}, EguiContext, EguiPlugin, };
+use bevy_egui::EguiPlugin;
 use taiju::chapter::prelude::*;
 use taiju::donut::Clock;
-use bevy::ecs::system::Command;
 
 mod runtime;
 mod editor;
@@ -17,14 +16,15 @@ fn main() {
     .init_resource::<runtime::Runtime>()
     .insert_resource(Clock::new())
     .add_startup_system(setup.system())
-    .add_system(ui_menu.system())
+    .add_startup_system(editor::Editor::spawn.system())
+    .add_system_to_stage(CoreStage::PreUpdate, editor::Editor::update_map.system())
+    .add_system_to_stage(CoreStage::Update, editor::Editor::update_ui.system())
     .run();
 }
 
 fn setup(
   mut commands: Commands,
   mut _rt: ResMut<runtime::Runtime>,
-  clock: Res<ClockRef>,
   mut texture_atlases: ResMut<Assets<TextureAtlas>>,
   mut color_materials: ResMut<Assets<ColorMaterial>>,
   asset_server: Res<AssetServer>,
@@ -36,77 +36,4 @@ fn setup(
   // cameras
   commands.spawn_bundle(OrthographicCameraBundle::new_2d());
   commands.spawn_bundle(UiCameraBundle::default());
-
-  // Editor
-  let map_id = commands
-    .spawn()
-    .insert(editor::Map)
-    .insert(Transform::identity())
-    .id();
-  commands.insert_resource(editor::Editor::new(map_id));
-}
-
-fn ui_menu(
-  mut commands: Commands,
-  egui_ctx: Res<EguiContext>,
-  mut editor: ResMut<editor::Editor>,
-  clock: Res<ClockRef>,
-  asset_server: Res<AssetServer>,
-) {
-  let input: &egui::InputState = egui_ctx.ctx().input();
-  for ev in input.events.iter() {
-    match ev {
-      &Event::Copy => {}
-      &Event::Cut => {}
-      Event::Text(str) => {}
-      &Event::Key {
-        key: Key,
-        pressed: bool,
-        modifiers: Modifiers,
-      } => {}
-      &Event::PointerMoved(pos) => {}
-      &Event::PointerButton {
-        pos,
-        button,
-        pressed,
-        modifiers,
-      } => {
-        if pressed {
-          if button == egui::PointerButton::Secondary {
-            editor.menu_pos = Some((pos.x, pos.y));
-          }
-        }
-      }
-      &Event::PointerGone => {}
-    }
-  }
-  egui::TopPanel::top("top_panel").show(egui_ctx.ctx(), |ui| {
-    egui::menu::bar(ui, |ui| {
-      egui::menu::menu(ui, "File", |ui| {
-        ui.label("Open Scene");
-        ui.indent("Open Source", |ui| {
-          if ui.button("Stage01").clicked() {
-            commands.remove_resource::<Handle<Scenario>>();
-            let handle = asset_server.load::<Scenario, _>("scenario/stage01.ron");
-            commands.insert_resource(handle);
-          }
-        });
-        ui.separator();
-        if ui.button("Save Scene").clicked() {
-        }
-        ui.separator();
-        if ui.button("Quit").clicked() {
-          std::process::exit(0);
-        }
-      });
-    });
-  });
-
-  if let Some(pos) = editor.menu_pos {
-    egui::Window::new("Hello")
-      .default_pos(pos)
-      .show(egui_ctx.ctx(), |ui| {
-        ui.label("world");
-      });
-  }
 }

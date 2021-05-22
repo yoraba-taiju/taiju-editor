43da75b69d289385d7380308e40989a4a98e4feb
Kaede Fujisaki <kaede@hexe.net>
2021-05-22 17:31:03 +0900

    remove unused imports

--- src/component/map/course/key_frame.rs
+++ src/component/map/course/key_frame.rs
@@ -1,7 +1,5 @@
 use bevy::{prelude::*, reflect::TypeUuid};
 
-use crate::bevy_render_primitive::{self, TriangleListBuilder};
-
 /******************************************************************************
  ** KeyFrame
  ******************************************************************************/
